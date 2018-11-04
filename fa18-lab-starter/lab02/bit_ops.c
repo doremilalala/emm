722b5f562573fff41248bb295018d37bf49af2c5
forio <forioop@yahoo.com>
2018-11-04 00:40:52 -0400

    bit_op

--- fa18-lab-starter/lab02/bit_ops.c
+++ fa18-lab-starter/lab02/bit_ops.c
@@ -4,23 +4,29 @@
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
+    x=x<<(31-n);
+    x=x>>31;
     // YOUR CODE HERE
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    return x;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
+    *x = ((*x>>(n+1)<<(n+1))+((
+        (*x<<(32-n)>>1)+(v<<31)
+        )>>(31-n)));
     // YOUR CODE HERE
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
+    set_bit(x,n,~get_bit(*x,n));
     // YOUR CODE HERE
 }
 
